b05ef0c337b59cf15f851b4fcc977778888110b2
Emily Hengemihle <hengemee@gmail.com>
2011-10-13 15:22:42 -0400

    Added in markup/css for main layout, questions, and footer

--- index.php
+++ index.php
@@ -21,56 +21,94 @@
 <body>
 	
 	<section id="ask">
-		
-		<header>
-
-			<h1>I need some haalp...</h1>
-
-		</header>
-		
-		<form id="ask-form" action="/" method="post"> 
+		<div class="subcontainer">
+			<header>
+	
+				<h1>I need some haalp...</h1>
+	
+			</header>
 			
-			<fieldset>
+			<form id="ask-form" action="/" method="post"> 
 				
-				<label for="halp">I need some haalp...</label>
-				<textarea id="halp" name="halp" tabindex="1"></textarea>
-				
-				<label for="time">Time</label>
-				<select id="time" multiple="" data-placeholder="48 Hours" tabindex="2">
+				<fieldset>
 					
-				<label for="tags">Tag this with</label>
-				<select data-placeholder="Uncategorized" class="chzn-select" multiple tabindex="3">
-		          	<option value=""></option> 
-		          	<option value="United States">United States</option> 
-		          	<option value="United Kingdom">United Kingdom</option>
-				</select>
+					<label for="halp">I need some haalp...</label>
+					<textarea id="halp" name="halp" tabindex="1"></textarea>
+					
+					<label for="time">Time</label>
+					<select id="time" multiple="" data-placeholder="48 Hours" tabindex="2">
+						
+					<label for="tags">Tag this with</label>
+					<select data-placeholder="Uncategorized" class="chzn-select" multiple tabindex="3">
+			          	<option value=""></option> 
+			          	<option value="United States">United States</option> 
+			          	<option value="United Kingdom">United Kingdom</option>
+					</select>
+					
+				</fieldset>
 				
-			</fieldset>
-			
-			<fieldset class="submit"> <!-- submit button -->
+				<fieldset class="submit"> <!-- submit button -->
+					
+					<input type="submit" name="submit" value="Submit Form" />
+					
+				</fieldset>
 				
-				<input type="submit" name="submit" value="Submit Form" />
-				
-			</fieldset>
-			
-		</form>
-		
+			</form>
+		</div>
 	</section>
 	
 	<section id="questions">
-		
-		<ul>
-			
-			<li>
-				
-			</li>
-			
-		</ul>
-		
+		<div class="subcontainer">
+			<nav>
+				<div class="article-sort">
+					Filter by:<select><option>Urgency</option></select>
+				</div>
+				<a href="#" class="more-haalps">See All Haalps &raquo;</a>
+			</nav>
+			<article>
+				<header>
+					<h3>2 Minutes Left</h3>
+					<ul><li><a href="#">sex</a></li><li><a href="#">nudity</a></li><li><a href="#">shameful</a></li></ul>
+				</header>
+				<p>The remake of Footloose arrives in theaters tomorrow, with Kenny Wormald taking over for Kevin Bacon as Ren McCormack. (The original: not as fun as you remember.) The 27-year old Wormald has been a professional dancer since he was a kid, growing up in Stoughton, Massachusetts, just outside of Boston. Last night, we caught up with him at the Footloose premiere after-party at the Tribeca Grand Hotel, and spoke with him about his dancing, his Boston accent, and his willingness to protect his co-star - and Ryan Seacrest's girlfriend - Julianne Hough from creeps.</p>
+			</article>
+			<article>
+				<header>
+					<h3>2 Minutes Left</h3>
+					<ul><li><a href="#">sex</a></li><li><a href="#">nudity</a></li><li><a href="#">shameful</a></li></ul>
+				</header>
+				<p>The remake of Footloose arrives in theaters tomorrow, with Kenny Wormald taking over for Kevin Bacon as Ren McCormack. (The original: not as fun as you remember.) The 27-year old Wormald has been a professional dancer since he was a kid, growing up in Stoughton, Massachusetts, just outside of Boston. Last night, we caught up with him at the Footloose premiere after-party at the Tribeca Grand Hotel, and spoke with him about his dancing, his Boston accent, and his willingness to protect his co-star - and Ryan Seacrest's girlfriend - Julianne Hough from creeps.</p>
+			</article>
+			<article>
+				<header>
+					<h3>2 Minutes Left</h3>
+					<ul><li><a href="#">sex</a></li><li><a href="#">nudity</a></li><li><a href="#">shameful</a></li></ul>
+				</header>
+				<p>The remake of Footloose arrives in theaters tomorrow, with Kenny Wormald taking over for Kevin Bacon as Ren McCormack. (The original: not as fun as you remember.) The 27-year old Wormald has been a professional dancer since he was a kid, growing up in Stoughton, Massachusetts, just outside of Boston. Last night, we caught up with him at the Footloose premiere after-party at the Tribeca Grand Hotel, and spoke with him about his dancing, his Boston accent, and his willingness to protect his co-star - and Ryan Seacrest's girlfriend - Julianne Hough from creeps.</p>
+			</article>
+			<article>
+				<header>
+					<h3>2 Minutes Left</h3>
+					<ul><li><a href="#">sex</a></li><li><a href="#">nudity</a></li><li><a href="#">shameful</a></li></ul>
+				</header>
+				<p>The remake of Footloose arrives in theaters tomorrow, with Kenny Wormald taking over for Kevin Bacon as Ren McCormack. (The original: not as fun as you remember.) The 27-year old Wormald has been a professional dancer since he was a kid, growing up in Stoughton, Massachusetts, just outside of Boston. Last night, we caught up with him at the Footloose premiere after-party at the Tribeca Grand Hotel, and spoke with him about his dancing, his Boston accent, and his willingness to protect his co-star - and Ryan Seacrest's girlfriend - Julianne Hough from creeps.</p>
+			</article>									
+		</div>
 	</section>
 	
 	<footer>
-		Footer
+		<div class="subcontainer">
+			<h2>Wait so whaaat???</h2>
+			<article>
+				<p>How did you get ready for the big angry dance scene in Footloose? You just think about anyone that's ever pissed you off your whole life and take it out on them.</p>
+			</article>
+			<article>
+				<p>Did you have a double? As far as the dancing goes? None. I did all of the dancing. The gymnastics flip on the pole, I can't take credit for that. But I mean, great editing. </p>
+			</article>
+			<article class="last">
+				<p>Is there a time when dancing should be outlawed? There is a time. Around Julianne Hough, if you're a creep. I can handle it... she, not so much.</p>
+			</article>
+		</div>
 	</footer>
 	
 <!-- here comes the javascript -->
